823723e66c1206be7615eae235b77a444e5b0d48
Pablo Domínguez Durán <pablo.mdd.007@gmail.com>
2020-11-06 23:02:55 -0600

    update files, add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <h1>About Route</h1>
+        </Route>
+        <Route path="/canvas">
+          <h1>Canvas Route</h1>
+        </Route>
+        <Route exact path="/">
+          <h1>Home Route</h1>
+        </Route>
+        <Route path="*">
+          <h1>Not found</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
