d1d02396715b8ed9a502f6c0be1c298615232a2f
Pablo Domínguez Durán <pablo.mdd.007@gmail.com>
2020-11-10 22:32:34 -0600

    Create new Hero  componetn

--- frontend/src/views/Home/index.jsx
+++ frontend/src/views/Home/index.jsx
@@ -8,6 +8,7 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import { Link as RouterLink } from "react-router-dom";
+import Hero from "../../components/Hero";
 
 const useStyles = makeStyles((theme) => ({
   mainFeaturedPost: {
@@ -42,54 +43,7 @@ function Home() {
   return (
     <>
       <Navbar />
-      <Container style={{"height":"60vh"}}>
-        <Paper
-          className={classes.mainFeaturedPost}
-          style={{
-            backgroundImage: `url(${"https://www.latercera.com/resizer/kHLHjR6u3jIRC7-xl_1oXBzxWUE=/800x0/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/MEP3EKODIVGKPDXNMGNLB6ZN3A.jpg"})`,
-          }}
-        >
-          {/* Increase the priority of the hero background image */}
-          {
-            <img
-              style={{ display: "none" }}
-              src={
-                "https://www.latercera.com/resizer/kHLHjR6u3jIRC7-xl_1oXBzxWUE=/800x0/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/MEP3EKODIVGKPDXNMGNLB6ZN3A.jpg"
-              }
-              alt={"Monet"}
-            />
-          }
-          <div className={classes.overlay} />
-          <Grid container>
-            <Grid item md={6}>
-              <div className={classes.mainFeaturedPostContent}>
-                <Typography
-                  component="h1"
-                  variant="h3"
-                  color="inherit"
-                  gutterBottom
-                >
-                  {"You are art. You are an artist"}
-                </Typography>
-                <Typography variant="h5" color="inherit" paragraph>
-                  {
-                    "With help of AI convert your favorite shot into a famous artist’s painting."
-                  }
-                </Typography>
-                <Button
-                  component={RouterLink}
-                  to={"/canvas"}
-                  color="secondary"
-                  //   variant="outlined"
-                  className={classes.link}
-                >
-                  Canvas
-                </Button>
-              </div>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Container>
+      <Hero />
       <Footer />
     </>
   );
