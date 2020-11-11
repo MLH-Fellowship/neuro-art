357eae9fa193f3637ae62113d0ffe31df00e6908
David Knox <dakdave26@gmail.com>
2020-11-11 16:14:01 -0500

    Added workaround for GPU support in this use case

--- backend/model/nst.py
+++ backend/model/nst.py
@@ -8,6 +8,16 @@ import time
 import functools
 import time
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs, ", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    print(e)
+
 def tensor_to_image(tf_input):
   '''
   Converts a numerical input into an RGB image.
